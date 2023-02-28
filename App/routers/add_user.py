df0d56b87d2b9f9ab4a8aa0410a2ac9bee326c5f
idrisabkar <idrisabkar2018@gmail.com>
2023-02-28 17:07:04 +0300

    new_user

--- App/routers/add_user.py
+++ App/routers/add_user.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.orm import Session
 from App.database import get_db
 from App.schemas import UserData
@@ -7,9 +7,12 @@ from App.models import Users
 route = APIRouter(tags=["User"])
 
 
-@route.post("/user")
+@route.post("/user", status_code=status.HTTP_200_OK)
 def add_user(data: UserData, db: Session = Depends(get_db)):
-    new_user = Users(**data.dict())
-    db.add(new_user)
-    db.commit()
-    return {"message": "add new User"}
+    try:
+        new_user = Users(**data.dict())
+        db.add(new_user)
+        db.commit()
+    except Exception:
+        raise HTTPException(status_code=status.HTTP_203_NON_AUTHORITATIVE_INFORMATION, detail=Exception)
+    return {"status": status.HTTP_200_OK, "message": "User Successfully Created"}
