3c1484ae703a2fafdfc42a2a8a2f17109227182e
idrisabkar <idrisabkar2018@gmail.com>
2023-02-28 20:30:10 +0300

    templates

--- WebApp/app.py
+++ WebApp/app.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    return render_template("index.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
 
 
 if __name__ == "__main__":
